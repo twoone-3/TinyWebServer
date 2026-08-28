0d06c99572649b35fc142ebf7d29fb7616fbba0d
twoone3 <twoone3141@gmail.com>
2026-08-28 09:44:42 +0800

    format with clangformat

--- webserver.h
+++ webserver.h
@@ -1,32 +1,32 @@
 #ifndef WEBSERVER_H
 #define WEBSERVER_H
 
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <cassert>
 #include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#include "./threadpool/threadpool.h"
+#include <cassert>
+
 #include "./http/http_conn.h"
+#include "./threadpool/threadpool.h"
 
-const int MAX_FD = 65536;           //最大文件描述符
-const int MAX_EVENT_NUMBER = 10000; //最大事件数
-const int TIMESLOT = 5;             //最小超时单位
+const int MAX_FD = 65536;            // 最大文件描述符
+const int MAX_EVENT_NUMBER = 10000;  // 最大事件数
+const int TIMESLOT = 5;              // 最小超时单位
 
-class WebServer
-{
-public:
+class WebServer {
+   public:
     WebServer();
     ~WebServer();
 
-    void init(int port , string user, string passWord, string databaseName,
-              int log_write , int opt_linger, int trigmode, int sql_num,
+    void init(int port, string user, string passWord, string databaseName,
+              int log_write, int opt_linger, int trigmode, int sql_num,
               int thread_num, int close_log, int actor_model);
 
     void thread_pool();
@@ -36,37 +36,37 @@ public:
     void eventListen();
     void eventLoop();
     void timer(int connfd, struct sockaddr_in client_address);
-    void adjust_timer(util_timer *timer);
-    void deal_timer(util_timer *timer, int sockfd);
+    void adjust_timer(util_timer* timer);
+    void deal_timer(util_timer* timer, int sockfd);
     bool dealclientdata();
     bool dealwithsignal(bool& timeout, bool& stop_server);
     void dealwithread(int sockfd);
     void dealwithwrite(int sockfd);
 
-public:
-    //基础
+   public:
+    // 基础
     int m_port;
-    char *m_root;
+    char* m_root;
     int m_log_write;
     int m_close_log;
     int m_actormodel;
 
     int m_pipefd[2];
     int m_epollfd;
-    http_conn *users;
+    http_conn* users;
 
-    //数据库相关
-    connection_pool *m_connPool;
-    string m_user;         //登陆数据库用户名
-    string m_passWord;     //登陆数据库密码
-    string m_databaseName; //使用数据库名
+    // 数据库相关
+    connection_pool* m_connPool;
+    string m_user;          // 登陆数据库用户名
+    string m_passWord;      // 登陆数据库密码
+    string m_databaseName;  // 使用数据库名
     int m_sql_num;
 
-    //线程池相关
-    threadpool<http_conn> *m_pool;
+    // 线程池相关
+    threadpool<http_conn>* m_pool;
     int m_thread_num;
 
-    //epoll_event相关
+    // epoll_event相关
     epoll_event events[MAX_EVENT_NUMBER];
 
     int m_listenfd;
@@ -75,8 +75,8 @@ public:
     int m_LISTENTrigmode;
     int m_CONNTrigmode;
 
-    //定时器相关
-    client_data *users_timer;
+    // 定时器相关
+    client_data* users_timer;
     Utils utils;
 };
 #endif
